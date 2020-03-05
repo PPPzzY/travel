779f6e4a69e79ad3238c206eeb6cdbce5e6f4c39
pppzzy0912 <1565847029@qq.com>
2020-03-05 20:28:26 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://imgs.qunarzz.com/sight/source/1505/9e/21df651e19af5d.jpg_r_640x214_3ea5bb38.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'https://imgs.qunarzz.com/sight/source/1505/aa/7baaf8a851d221.jpg_r_640x214_1431200f.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+  .wrapper>>>.swiper-pagination-bullet-active
+    background #fff
+  .wrapper
+    overflow hidden
+    width 100%
+    height 0
+    padding-bottom 36.25%
+    background #eee
+  .swiper-img
+    width 100%
+</style>
