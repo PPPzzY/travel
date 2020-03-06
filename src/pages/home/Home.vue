c922d28fd8e2caff86ea933d642b1aa2a59b4bc8
pppzzy0912 <1565847029@qq.com>
2020-03-06 21:59:29 +0800

    city-ajax

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -36,22 +36,22 @@ export default {
   },
   methods: {
     getHomeInfo () {
-      axios.get('/static/mock/index.json').then(res => {
-        res = res.data
-        if (res.ret && res.data) {
-          const data = res.data
-          this.city = data.city
-          this.swiperList = data.swiperList
-          this.iconList = data.iconList
-          this.recommendList = data.recommendList
-          this.weekendList = data.weekendList
-        }
-      }).catch(err => {
-        console.log(err)
-      })
+      axios.get('/static/mock/index.json')
+        .then(this.getHomeInfoSucc)
+        .catch(err => {
+          console.log(err)
+        })
     },
     getHomeInfoSucc (res) {
-      console.log(res)
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.city = data.city
+        this.swiperList = data.swiperList
+        this.iconList = data.iconList
+        this.recommendList = data.recommendList
+        this.weekendList = data.weekendList
+      }
     }
   },
   mounted () {
